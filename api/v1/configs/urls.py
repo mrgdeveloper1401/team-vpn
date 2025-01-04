177507fe98922304fe8c6cc2ccf3d060175e8a05
mohammad goodarzi <m.goodarzi606@gmail.com>
2025-01-04 15:19:57 +0330

    remove free_config in url

--- api/v1/configs/urls.py
+++ api/v1/configs/urls.py
@@ -7,7 +7,7 @@ from . import views
 app_name = 'configs'
 router = routers.DefaultRouter()
 router.register(r'country', views.CountryViewSet, basename="country")
-router.register('free_config', views.FreeConfigViewSet, basename="free_config")
+# router.register('free_config', views.FreeConfigViewSet, basename="free_config")
 
 country = routers.NestedDefaultRouter(router, r'country', lookup='country')
 country.register(r'config', views.ConfigViewSet, basename="config")
