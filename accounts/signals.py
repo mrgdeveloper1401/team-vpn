5f7bf126ac3034993605cc1066c21c8c66c71e9f
mohammad goodarzi <m.goodarzi606@gmail.com>
2025-01-06 22:44:47 +0330

    delete comment in file singles.py in app accounts

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,12 +1,9 @@
 from django.dispatch import receiver
 from django.contrib.auth.signals import user_logged_in
-from django.db.models.signals import pre_save
-from rest_framework.exceptions import PermissionDenied, ValidationError
+from rest_framework.exceptions import PermissionDenied
 from axes.signals import user_locked_out
-from rest_framework import status
 
 from accounts.models import User
-
 
 
 @receiver(user_locked_out)
@@ -19,10 +16,3 @@ def increase_number_of_login(sender, request, user, **kwargs):
     if isinstance(user, User):
         user.number_of_login += 1
         user.save()
-
-
-# @receiver(pre_save, sender=User)
-# def check_maximum_device(sender, instance, **kwargs):
-#     if instance.user_device.count() > instance.number_of_device:
-#         raise ValidationError({'detail': "your account max device connection has arrived"},
-#                               code=status.HTTP_403_FORBIDDEN)
