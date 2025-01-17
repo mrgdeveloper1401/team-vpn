586a3f7adb88f4db110b5101e9f4244b62a5d0f3
mohammad goodarzi <m.goodarzi606@gmail.com>
2025-01-17 10:20:46 +0330

    add class PointTest for User

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,32 @@
-from django.test import TestCase
+import logging
 
-# Create your tests here.
+import pytest
+from django.test import TestCase, Client, override_settings
+
+
+@pytest.fixture(autouse=True)
+def disable_logging():
+    logging.disable(logging.CRITICAL)
+
+
+@pytest.fixture
+def maintain_mode_on(settings):
+    settings.MAINTENANCE_MODE = True
+
+
+@pytest.fixture
+def maintain_mode_off(settings):
+    settings.MAINTENANCE_MODE = False
+
+
+def test_response_when_maintenance_mode_is_on(maintain_mode_on, client):
+    response = client.get("/")
+    response_text = "application maintenance mode, please try again"
+    assert response.content.decode() == response_text
+
+
+@override_settings(MAINTENANCE_MODE=False)
+def test_response_when_maintenance_mode_is_off(self):
+    response = self.client.get("/")
+    self.assertContains(response, "Maintenance mode is off")
+    self.assertTemplateUsed(response, "maintenance_mode.html")
---
+++ tests/tests_account.py
@@ -0,0 +1,40 @@
+from django.test import TestCase
+from django.utils import timezone
+
+from accounts.models import User
+
+
+class PointTest(TestCase):
+    def setUp(self):
+        point_user = User.objects.create_user(username='test_user', email='test@gmail.com', password='<PASSWORD>',
+                                              first_name='test',
+                                              last_name='tests')
+        self.test_user = point_user
+
+    def test_normal_user(self):
+        self.assertEqual(self.test_user.account_type, 'normal_user')
+
+    def test_accounts_is_active(self):
+        """
+        account is active and user not deleted
+        """
+        self.assertTrue(self.test_user.is_active)
+
+    def test_accounts_is_not_deleted(self):
+        self.assertEqual(self.test_user.is_deleted, None)
+        self.assertEqual(self.test_user.deleted_at, None)
+
+    def test_accounts_status(self):
+        self.assertEqual(self.test_user.accounts_status, 'limit')
+
+    def test_get_user_information(self):
+        self.assertEqual(self.test_user.first_name, "test")
+        self.assertEqual(self.test_user.last_name, "tests")
+        self.assertEqual(self.test_user.email, "test@gmail.com")
+        self.assertEqual(self.test_user.username, "test_user")
+        self.assertEqual(self.test_user.start_premium, timezone.localdate())
+        self.assertEqual(self.test_user.number_of_max_device, 1)
+        self.assertEqual(self.test_user.last_login, None)
+        self.assertEqual(self.test_user.volume, 0)
+        self.assertEqual(self.test_user.volume_usage, 0)
+        self.assertFalse(self.test_user.is_connected_user)
