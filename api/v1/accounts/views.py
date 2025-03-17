1fe42aac94d6fea5307ea50a8d97875c03da06b5
mohammad goodarzi <m.goodarzi606@gmail.com>
2025-03-17 05:33:20 +0330

    edit views

--- api/v1/accounts/views.py
+++ api/v1/accounts/views.py
@@ -1,16 +1,20 @@
 from django.contrib.auth import authenticate
-from rest_framework import viewsets, status, permissions
+from rest_framework import viewsets, status
 from rest_framework import mixins
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import views
 from rest_framework.response import Response
+from django.db.models import F
+from django.http import HttpResponse
 
-from accounts.models import User, ContentDevice, PrivateNotification
-from vpn.utils.create_refresh_token import get_token_refresh_token
-from vpn.utils.paginations import CommonPagination, AdminUserProfilePagination
-from vpn.utils.permissions import NotAuthenticated
+from dj_vpn.accounts.enums import AccountStatus
+from dj_vpn.accounts.models import User, ContentDevice, PrivateNotification
+from dj_vpn.vpn.utils.create_refresh_token import get_token_refresh_token
+from dj_vpn.vpn.utils.paginations import CommonPagination
+from dj_vpn.vpn.utils.permissions import NotAuthenticated
 from . import serializers
-from .serializers import ContentDeviceSerializer, PrivateNotificationsSerializer
+from .serializers import ContentDeviceSerializer, PrivateNotificationsSerializer, VolumeUsageSerializer
+from dj_vpn.vpn.utils.status_code import ErrorResponse
 
 
 class UserRegisterViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
@@ -19,33 +23,36 @@ class UserRegisterViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     permission_classes = [NotAuthenticated]
 
 
-class UserProfileViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
-                         mixins.DestroyModelMixin, viewsets.GenericViewSet):
+class UserProfileViewSet(mixins.UpdateModelMixin, mixins.RetrieveModelMixin,
+                         viewsets.GenericViewSet):
     queryset = User.objects.all()
     permission_classes = [IsAuthenticated]
-    serializer_class = serializers.AdminUserProfileSerializer
-    pagination_class = AdminUserProfilePagination
 
     def get_queryset(self):
-        if self.request.user.is_staff:
-            return self.queryset
-        if 'pk' in self.kwargs and self.request.user.is_staff:
-            return self.queryset.filter(id=self.kwargs['pk'])
-        if 'pk' in self.kwargs:
-            return self.queryset.filter(id=self.request.user.id)
         return self.queryset.filter(id=self.request.user.id)
-    
-    def get_permissions(self):
-        if self.request.method == 'DELETE':
-            return [IsAdminUser()]
-        return super().get_permissions()
 
     def get_serializer_class(self):
         if self.action in ["list", 'retrieve']:
-            return serializers.ListUserProfileSerializer
+            return serializers.GetUserProfileSerializer
         if self.action in ['update', 'partial_update']:
             return serializers.UpdateUserProfileSerializer
         return super().get_serializer_class()
+
+
+class UserProfileApiView(views.APIView):
+    serializer_class = serializers.GetUserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, *args, **kwargs):
+        query = User.objects.get(username=request.user.username)
+        serializer = self.serializer_class(query)
+        if query.accounts_status == AccountStatus.LIMIT:
+            response = Response(data={"message": "limit for today reached"}, status=status.HTTP_403_FORBIDDEN)
+        elif query.accounts_status == AccountStatus.EXPIRED:
+            response = Response(data={"message": "usage limit reached"}, status=status.HTTP_403_FORBIDDEN)
+        else:
+            response = Response(serializer.data, status=status.HTTP_200_OK)
+        return response
 
 
 class LoginApiView(views.APIView):
@@ -69,36 +76,27 @@ class LoginApiView(views.APIView):
                 key='token', value=str(refresh), httponly=True, secure=True, samesite='Lax'
             )
             user.number_of_login += 1
+            user.fcm_token = serializer.validated_data['fcm_token']
             user.save()
             return response
         return Response({"detail": "invalid input"}, status=status.HTTP_400_BAD_REQUEST)
 
 
-class ContentDeviceViewSet(viewsets.ModelViewSet):
+class ContentDeviceViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = ContentDeviceSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return ContentDevice.objects.filter(user=self.request.user)
 
-    # def get_permissions(self):
-    #     if self.request.method not in permissions.SAFE_METHODS:
-    #         return [IsAdminUser()]
-    #     return super().get_permissions()
 
-
-class PrivateNotificationViewSet(viewsets.ModelViewSet):
+class PrivateNotificationViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     serializer_class = PrivateNotificationsSerializer
     permission_classes = [IsAuthenticated]
     pagination_class = CommonPagination
 
     def get_queryset(self):
         return PrivateNotification.objects.filter(user=self.request.user)
-
-    def get_permissions(self):
-        if self.request.method not in permissions.SAFE_METHODS:
-            return [IsAdminUser()]
-        return super().get_permissions()
 
 
 # class LogoutApiView(viewsets.GenericViewSet, mixins.CreateModelMixin):
@@ -107,3 +105,56 @@ class PrivateNotificationViewSet(viewsets.ModelViewSet):
 
 # def show_request(request):
 #     return request
+
+
+class VolumeUsageApiView(views.APIView):
+    serializer_class = VolumeUsageSerializer
+    # permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = User.objects.filter(username=serializer.validated_data['username']).only("username")
+        if user:
+            if user.first().accounts_status == AccountStatus.ACTIVE:
+                user.update(
+                    volume_usage=F('volume_usage') + serializer.validated_data['volume_usage'],
+                    all_volume_usage=F("all_volume_usage") + serializer.validated_data['volume_usage'],
+                )
+                return HttpResponse(content="ok", content_type="text/plain")
+            return HttpResponse("DISCONNECT", status=status.HTTP_400_BAD_REQUEST, content_type="text/plain")
+        return HttpResponse("USER NOT FOUND", status=status.HTTP_400_BAD_REQUEST, content_type="text/plain")
+
+
+class UpdateConnectionApiView(views.APIView):
+    """
+    this api update field is_connected_user
+    you have error 400, when two something was happened
+    if a user account is not active
+    if field is_connected_user is active
+    """
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        if user.accounts_status == AccountStatus.ACTIVE and not user.is_connected_user:
+            user.is_connected_user = True
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        return Response(ErrorResponse.USER_USAGE_LIMIT, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeactivateUserConnectionApiView(views.APIView):
+    """
+    this api deactivate field is_connected_user
+    """
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        if user.is_connected_user:
+            user.is_connected_user = False
+            user.save()
+            return Response(status=status.HTTP_200_OK)
+        return Response(ErrorResponse.FIELD_NOT_ACTIVE)
